456042c8f2821e2d2c0e8bee629f7a79657a5f76
chris <butchchr@gmail.com>
2018-07-20 15:46:44 -0400

    DONE!

--- gccapstone1/Program.cs
+++ gccapstone1/Program.cs
@@ -10,27 +10,46 @@ namespace gccapstone1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello user I am a Pig Latin converter: ");
-            string userWords = Console.ReadLine();
-
-            if (userWords.StartsWith("a") || userWords.StartsWith("e") || userWords.StartsWith("i") || userWords.StartsWith("o") || userWords.StartsWith("u"))
+            bool y = true;
+            while (y)
             {
-                Console.WriteLine(userWords + "way");
+                
+                char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+                string firstBit = "";
+                string firstVowel = "";
+                Console.WriteLine("Hello user I am a Pig Latin converter: ");
+                string userWords = Console.ReadLine();
+
+                if (userWords.StartsWith("a") || userWords.StartsWith("e") || userWords.StartsWith("i") || userWords.StartsWith("o") || userWords.StartsWith("u"))
+                {
+                    Console.WriteLine(userWords + "way");
+                }
+                
+                else for (int i = 0; i < userWords.Length; i++)
+                    {
+
+                        string[] words = userWords.Split(vowels, 2);
+
+                        firstBit = userWords.Substring(0, 1);
+                        firstVowel = userWords.Substring(1, userWords.Length - 1);
+
+                    }
+                Console.WriteLine(firstVowel + firstBit + "ay");
+
+                //continue y/n
+                bool invalid = true;
+                while (invalid)
+                {
+                    Console.WriteLine("Continue? (y/n):");
+                    ConsoleKeyInfo pressed = Console.ReadKey();
+                    Console.WriteLine();
+                    bool isY = pressed.Key == ConsoleKey.Y;
+                    bool isN = pressed.Key == ConsoleKey.N;
+
+                    invalid = !isY && !isN;
+                    y = isY;
+                }
             }
-            string i
-            else (i = 0; i < userWords.Length; i++)
-            {
-                substring a;
-                substring b;
-
-                +ay to substring a cwl sub b + substring a 
-            }
-
-            
-
-
-            Console.ReadKey();
-
         }
     }
 }
