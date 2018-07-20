db70c7c1a5f2d90ac3d8498741b0f8135b0fc214
chris <butchchr@gmail.com>
2018-07-20 14:20:32 -0400

    did some stuff

--- gccapstone1/Program.cs
+++ gccapstone1/Program.cs
@@ -10,6 +10,27 @@ namespace gccapstone1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello user I am a Pig Latin converter: ");
+            string userWords = Console.ReadLine();
+
+            if (userWords.StartsWith("a") || userWords.StartsWith("e") || userWords.StartsWith("i") || userWords.StartsWith("o") || userWords.StartsWith("u"))
+            {
+                Console.WriteLine(userWords + "way");
+            }
+            string i
+            else (i = 0; i < userWords.Length; i++)
+            {
+                substring a;
+                substring b;
+
+                +ay to substring a cwl sub b + substring a 
+            }
+
+            
+
+
+            Console.ReadKey();
+
         }
     }
 }
