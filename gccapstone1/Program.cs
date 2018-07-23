bf6f4a647e8a8a6f369fcb52e2f0915947f66fb8
chris <butchchr@gmail.com>
2018-07-22 23:04:45 -0400

    really done this time not fake done like last time

--- gccapstone1/Program.cs
+++ gccapstone1/Program.cs
@@ -8,33 +8,43 @@ namespace gccapstone1
 {
     class Program
     {
+        const char a = 'a';
+        const char e = 'e';
+        const char i = 'i';
+        const char o = 'o';
+        const char u = 'u';
+
+        static readonly char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
         static void Main(string[] args)
         {
             bool y = true;
             while (y)
             {
-                
-                char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
-                string firstBit = "";
-                string firstVowel = "";
-                Console.WriteLine("Hello user I am a Pig Latin converter: ");
+                //input and puts to lower
+                Console.WriteLine("Hello user I am a Pig Latin converter, please enter a word: ");
                 string userWords = Console.ReadLine();
+                string lowerWords = userWords.ToLower();
 
-                if (userWords.StartsWith("a") || userWords.StartsWith("e") || userWords.StartsWith("i") || userWords.StartsWith("o") || userWords.StartsWith("u"))
+                //checks to make sure the word has a a vowel and is not null or whitespace
+                if (IsValid(lowerWords) && HasVowel(lowerWords))
                 {
-                    Console.WriteLine(userWords + "way");
-                }
-                
-                else for (int i = 0; i < userWords.Length; i++)
+                    //checks to see if it starts with a vowel and adds way 
+                    if (StartsWithVowel(lowerWords))
                     {
-
-                        string[] words = userWords.Split(vowels, 2);
-
-                        firstBit = userWords.Substring(0, 1);
-                        firstVowel = userWords.Substring(1, userWords.Length - 1);
-
+                        Console.WriteLine(lowerWords + "way");
                     }
-                Console.WriteLine(firstVowel + firstBit + "ay");
+
+                    //since we know the word does not start with a vowel, but has one, it goes here to get converted to pig latin 
+                    else
+                    {
+                        Console.WriteLine(StartsWithCons(lowerWords));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Input is not valid");
+                }
 
                 //continue y/n
                 bool invalid = true;
@@ -50,6 +60,43 @@ namespace gccapstone1
                     y = isY;
                 }
             }
+        }
+
+        //method to check for null or white space
+        static bool IsValid(string lowerWords)
+        {
+            return !string.IsNullOrWhiteSpace(lowerWords);
+        }
+
+        //method to check if it has a vowel
+        static bool HasVowel(string lowerWords)
+        {
+            return (lowerWords.Contains(a) || lowerWords.Contains(e) || lowerWords.Contains(i) || lowerWords.Contains(o) || lowerWords.Contains(u));
+        }
+
+        //method to check if it starts with a vowel
+        static bool StartsWithVowel(string lowerWord)
+        {
+            return (lowerWord.StartsWith("a") || lowerWord.StartsWith("e") || lowerWord.StartsWith("i") || lowerWord.StartsWith("o") || lowerWord.StartsWith("u"));
+        }
+
+        //method to convert a word not starting with a vowel to pig latin
+        static string StartsWithCons(string lowerWord)
+        {
+            int indexOfFirstVowel = lowerWord.Length + 1;
+            for (int i = 0; i < vowels.Length; i++)
+            {
+                int indexOfVowel = lowerWord.IndexOf(vowels[i]);
+                if (indexOfVowel > -1 && indexOfVowel < indexOfFirstVowel)
+                {
+                    indexOfFirstVowel = indexOfVowel;
+                }
+            }
+            
+            string firstBit = lowerWord.Substring(0, indexOfFirstVowel);
+            string secondBit = lowerWord.Substring(indexOfFirstVowel);
+
+            return secondBit + firstBit + "ay";
         }
     }
 }
